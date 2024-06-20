b6926169eabd85d19813224d27364ad4b5e4f8c7
Luc Perkins <lucperkins@gmail.com>
2024-06-19 17:13:36 -0700

    Make the CLI build everything by default

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::{collections::HashMap, process::exit};
+use std::collections::HashMap;
 
 use clap::Parser;
 use serde::Deserialize;
@@ -16,14 +16,11 @@ struct FlakeOutputs {
 
 #[derive(Debug, Parser)]
 struct Cli {
-    #[arg(short = 'd', long, default_value_t = false)]
+    #[arg(short = 'd', long, default_value_t = true)]
     dev_shells: bool,
 
-    #[arg(short = 'p', long, default_value_t = false)]
+    #[arg(short = 'p', long, default_value_t = true)]
     packages: bool,
-
-    #[arg(short = 'b', long, default_value_t = false)]
-    build: bool,
 }
 
 fn get_nix_system() -> String {
@@ -45,16 +42,18 @@ fn nix_build(output: &str) {
         .expect("couldn't get command output");
 }
 
-fn main() {
+#[derive(Debug, thiserror::Error)]
+enum FlakeIterError {}
+
+fn main() -> Result<(), FlakeIterError> {
     let Cli {
         dev_shells,
         packages,
-        build,
     } = Cli::parse();
 
-    if !build || (!dev_shells && !packages) {
+    if !dev_shells && !packages {
         println!("Nothing to build");
-        exit(1);
+        return Ok(());
     }
 
     let cmd_output = String::from_utf8(
@@ -71,8 +70,23 @@ fn main() {
 
     let system = get_nix_system();
 
-    if dev_shells && build {
-        println!("Building dev shell outputs\n");
+    if packages {
+        println!("Building package outputs");
+        for (sys, pkg) in outputs.packages {
+            for (name, _) in pkg {
+                if sys == system {
+                    let output = format!(".#packages.{system}.{name}");
+                    println!("Building package output {name}");
+                    nix_build(&output);
+                    println!("Successfully built package {name}");
+                }
+            }
+        }
+        println!("Finished building package outputs");
+    }
+
+    if dev_shells {
+        println!("Building dev shell outputs");
         for (sys, shell) in outputs.dev_shells {
             for (name, _) in shell {
                 if sys == system {
@@ -83,19 +97,8 @@ fn main() {
                 }
             }
         }
+        println!("Finished building dev shell outputs");
     }
 
-    if packages && build {
-        println!("Building package outputs\n");
-        for (sys, pkg) in outputs.packages {
-            for (name, _) in pkg {
-                if sys == system {
-                    let output = format!(".#packages.{system}.{name}");
-                    println!("Building package {name}");
-                    nix_build(&output);
-                    println!("Successfully built package {name}");
-                }
-            }
-        }
-    }
+    Ok(())
 }
