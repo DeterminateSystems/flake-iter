317aaf5ab2bb00d429796ca358d352a7a1800a10
Luc Perkins <lucperkins@gmail.com>
2024-10-26 11:00:28 +0200

    Hard-code CI issues URL

--- src/cli.rs
+++ src/cli.rs
@@ -50,7 +50,8 @@ impl Cli {
         let default_log_level = if verbose { Level::DEBUG } else { Level::INFO };
 
         color_eyre::config::HookBuilder::default()
-            .issue_url(concat!(env!("CARGO_PKG_REPOSITORY"), "/issues/new"))
+            // flake-iter is a private repo so we direct people to the ci repo for reporting issues
+            .issue_url("https://github.com/DeterminateSystems/ci/issues/new")
             .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
             .add_issue_metadata("os", std::env::consts::OS)
             .add_issue_metadata("arch", std::env::consts::ARCH)
